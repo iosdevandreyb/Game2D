7b2470118eb61f3d7ea3dd9cf9c2dd3c4a9de03d
Andrei <bojieitsit@icloud.com>
2023-10-10 21:19:08 +0400

    Settings controller created

---
+++ Game2D/Controllers/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Game2D
+//
+//  Created by Andrei Bogoslovskii on 10.10.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
